4ea446f30035ccda6a184955dd42eb9334cd8d47
Daniel W. Robert <danielwrobert@gmail.com>
2013-08-03 07:58:51 -0700

    added proj obj and scrolling method

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+var lovers = {
+	navSlide : function($link) {
+		var target = $link.attr("href");
+		$link.on("click", function() {
+			$("html, body").animate({ scrollTop : target }, 1000).offset.top();
+		});
+	}
+};
