9466401b17a35ef594f46a85baf25ed8e9b0fa79
Daniel W. Robert <danielwrobert@gmail.com>
2013-08-12 08:38:30 -0700

    added grunt config. added sliding nav and matchmedia polyfill

---
+++ Gruntfile.js
@@ -0,0 +1,120 @@
+/*
+ * grunt
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2013 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
+ */
+
+'use strict';
+
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/** \n' +
+      ' * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %> \n' +
+      ' * web: <%= pkg.homepage %> \n' +
+      ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> \n */\n',
+    footer: '\n',
+
+    // Setup Uglify
+    //    To run sepatate files, list files as shown below (https://github.com/gruntjs/grunt-contrib-uglify/issues/23)
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */',
+        mangle: false
+      },
+      dist: {
+        files: {
+          'js/main.min.js': ['js/main.js'],
+          'js/plugins.min.js': ['js/plugins.js']
+        }
+      }
+    },
+
+    // Setup jshint to behave
+    jshint: {
+      files: ['gruntfile.js', 'js/main.js'],
+      options: {
+        "curly": true,
+        "eqeqeq": true,
+        "immed": true,
+        "latedef": true,
+        "newcap": true,
+        "noarg": true,
+        "sub": true,
+        // "undef": true,
+        "unused": true,
+        "boss": true,
+        "eqnull": true,
+        "node": true,
+        globals: {
+          jQuery: true,
+        }
+      }
+    },
+
+    // Setup Watch
+    //    Lists of files & tasks as seen below
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      compass: {
+        files: 'sass/partials/*.scss',
+        tasks: ['compass']
+      },
+      uglify: {
+        files: ['js/main.js', 'js/plugins.js'],
+        tasks: ['uglify']
+      }
+    },
+
+    // Setup Compass/Sass to load from existing config.rb
+    //    Seems that sassDir & cssDir still needs to be defined regardless. Just copied from config.rb
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass/',
+          cssDir: '.',
+          config: 'src/config.rb'
+        }
+      }
+    }
+
+    // Setup ImageMin
+    // imagemin: {
+    //   dist: {
+    //     files: {
+    //       'img-optim/*.png': 'img/*.png',
+    //       'img-optim/*.jpg': 'img/*.jpg'
+    //     }
+    //   }
+    // }
+  });
+
+  // Load required tasks
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  // grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Set Default Tasks
+  // $ Grunt
+  // Test/Minify JS and Compile Sass
+  grunt.registerTask('default', [
+    'jshint',
+    'compass',
+    // 'imagemin',
+    'uglify'
+  ]);
+};
--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
-var lovers = {
-	navSlide : function($link) {
-		var target = $link.attr("href");
-		$link.on("click", function() {
-			$("html, body").animate({ scrollTop : target }, 1000).offset.top();
-		});
-	}
-};
+/*
+	Author: Daniel Robert
+*/
+
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
+window.matchMedia = window.matchMedia || (function( doc, undefined ) {
+
+  "use strict";
+
+  var bool,
+      docElem = doc.documentElement,
+      refNode = docElem.firstElementChild || docElem.firstChild,
+      // fakeBody required for <FF4 when executed in <head>
+      fakeBody = doc.createElement( "body" ),
+      div = doc.createElement( "div" );
+
+  div.id = "mq-test-1";
+  div.style.cssText = "position:absolute;top:-100em";
+  fakeBody.style.background = "none";
+  fakeBody.appendChild(div);
+
+  return function(q){
+
+    div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
+
+    docElem.insertBefore( fakeBody, refNode );
+    bool = div.offsetWidth === 42;
+    docElem.removeChild( fakeBody );
+
+    return {
+      matches: bool,
+      media: q
+    };
+
+  };
+
+}( document ));
+
+
+// Wrap in IIFE to protect namespaces
+(function($) {
+	var lovers = {
+		navSlide : function($link) {
+			$link.on("click", function() {
+				var target = $(this).attr("href");
+				$("html, body").animate({ scrollTop : $(target).offset().top }, 1000);
+				return false;
+			});
+		}
+	};
+
+	var $navLinks = $("nav a");
+	lovers.navSlide($navLinks);
+})(jQuery);
