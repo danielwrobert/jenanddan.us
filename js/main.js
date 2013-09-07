45ad86fae1242775aacaccb1612d38b5502ade1d
Daniel W. Robert <danielwrobert@gmail.com>
2013-09-07 10:18:06 -0700

    updated js spacing/indentation

--- js/main.js
+++ js/main.js
@@ -5,34 +5,34 @@
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
 window.matchMedia = window.matchMedia || (function( doc, undefined ) {
 
-  "use strict";
+    "use strict";
 
-  var bool,
-      docElem = doc.documentElement,
-      refNode = docElem.firstElementChild || docElem.firstChild,
-      // fakeBody required for <FF4 when executed in <head>
-      fakeBody = doc.createElement( "body" ),
-      div = doc.createElement( "div" );
+    var bool,
+        docElem = doc.documentElement,
+        refNode = docElem.firstElementChild || docElem.firstChild,
+        // fakeBody required for <FF4 when executed in <head>
+        fakeBody = doc.createElement( "body" ),
+        div = doc.createElement( "div" );
 
-  div.id = "mq-test-1";
-  div.style.cssText = "position:absolute;top:-100em";
-  fakeBody.style.background = "none";
-  fakeBody.appendChild(div);
+    div.id = "mq-test-1";
+    div.style.cssText = "position:absolute;top:-100em";
+    fakeBody.style.background = "none";
+    fakeBody.appendChild(div);
 
-  return function(q){
+    return function(q){
 
-    div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
+        div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
 
-    docElem.insertBefore( fakeBody, refNode );
-    bool = div.offsetWidth === 42;
-    docElem.removeChild( fakeBody );
+        docElem.insertBefore( fakeBody, refNode );
+        bool = div.offsetWidth === 42;
+        docElem.removeChild( fakeBody );
 
-    return {
-      matches: bool,
-      media: q
+        return {
+            matches: bool,
+            media: q
+        };
+
     };
-
-  };
 
 }( document ));
 
@@ -40,31 +40,31 @@ window.matchMedia = window.matchMedia || (function( doc, undefined ) {
 // Wrap in IIFE to protect namespaces
 (function($) {
 	var lovers = {
-    addMenuBtn : function(siblingList) {
-      $("<a>", {
-          class : "menu",
-          href : "#",
-          text : "MENU",
-          click : function() {
-            $(this).next().slideToggle(200);
-            return false;
-          }
-      }).insertBefore(siblingList);
-    },
-		pageNav : function(navList) {
-      var $navLinks = $(navList+" a");
+        addMenuBtn : function(siblingList) {
+            $("<a>", {
+                class : "menu",
+                href : "#",
+                text : "MENU",
+                click : function() {
+                    $(this).next().slideToggle(200);
+                    return false;
+                }
+            }).insertBefore(siblingList);
+        },
+        pageNav : function(navList) {
+            var $navLinks = $(navList+" a");
 
-      // Generate menu button is page is rendered below 760px width
-      if (matchMedia('screen and (max-width: 47.5em)').matches) {
-        this.addMenuBtn(navList);
-      }
-      //When nav button is clicked, scroll page to target section
-      $navLinks.on("click", function() {
-        var target = $(this).attr("href");
-        $("html, body").animate({ scrollTop : $(target).offset().top }, 1000);
-        $(".menu").trigger("click");
-        return false;
-      });
+            // Generate menu button is page is rendered below 760px width
+            if (matchMedia('screen and (max-width: 47.5em)').matches) {
+                this.addMenuBtn(navList);
+            }
+            //When nav button is clicked, scroll page to target section
+            $navLinks.on("click", function() {
+                var target = $(this).attr("href");
+                $("html, body").animate({ scrollTop : $(target).offset().top }, 1000);
+                $(".menu").trigger("click");
+                return false;
+            });
 		}
 	};
 
