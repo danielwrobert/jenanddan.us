274493aaded3fc37326814b711344252567b1bd0
Daniel W. Robert <danielwrobert@gmail.com>
2017-10-21 16:31:36 -0400

    Added modal styles and functionality to photo gallery.w

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -30,6 +30,54 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.modal {
+    align-items: center;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    left: 0;
+    position: fixed;
+    top: 0;
+    width: 100vw;
+}
+.modal-background {
+    background: rgba(10, 10, 10, 0.86);
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+}
+.modal-content {
+    position: relative;
+    z-index: 1;
+
+    img {
+        border: none;
+        max-height: calc(100vh - 40px);
+    }
+}
+.modal-close {
+    background: none;
+    border: none;
+    border-radius: 50px;
+    color: #fff;
+    cursor: pointer;
+    flex-grow: 0;
+    flex-shrink: 0;
+    font-size: 1.6rem;
+    height: 32px;
+    position: fixed;
+    right: 20px;
+    top: 20px;
+    user-select: none;
+    width: 32px;
+    z-index: 2;
+
+    &:hover {
+        background: rgba(10, 10, 10, 0.96);
+    }
+}
 
 </style>
